17c9c7be78a15a623d8773cf2602d154be3b5bbf
IMG-2017\Stud <stud@ase.ro>
2022-10-04 17:48:35 +0300

    seminar 2

---
+++ fisier.js
@@ -0,0 +1,9 @@
+/*console.log(process.argv[0]);
+console.log(process.argv[1]);
+console.log(process.argv[2]);*/
+
+let sayHello = function (name) {
+    return "Hello, " + name;
+}
+
+console.log(sayHello("me"));
---
+++ newindex.js
@@ -0,0 +1,31 @@
+/*const express = require("express")
+const req = require("express/lib/request")
+
+const app = express()
+
+app.get('/ping', (req, res) => {
+    res.send('pong')
+})
+
+app.listen(8080)*/
+ console.log(x)
+ let x="text"
+ console.log(x)
+var number = 1;
+let bool = true;
+const string = "HELLO";
+
+let obj = {
+    key: "object"
+}
+
+var d = function datatype () {
+    return true;
+}
+
+console.log("Must be number: ", typeof number);
+console.log("Must be boolean: ", typeof bool);
+console.log("Must be string: ", typeof string);
+console.log("Must be object: ", typeof obj);
+console.log("Must be function: ", typeof d);
+
